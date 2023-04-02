5adb7dfd9c6dacc82bedaf2548b248c97da24344
HutaJoullach <hutajoullach@gmail.com>
2023-04-02 09:04:17 +0900

    added user data to a list of objects in objects router

--- src/server/api/routers/objects.ts
+++ src/server/api/routers/objects.ts
@@ -1,18 +1,33 @@
+import type { User } from "@clerk/nextjs/dist/api";
 import { clerkClient } from "@clerk/nextjs/server";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    username: user.username,
+    profileImageUrl: user.profileImageUrl,
+  };
+};
 
 export const objectsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
     const objects = await ctx.prisma.object.findMany({
       take: 100,
     });
-    const users = await clerkClient.users.getUserList({
-      userId: objects.map((object) => object.authorId),
-      limit: 100,
-    });
-    console.log(users);
-    return objects;
+    const users = (
+      await clerkClient.users.getUserList({
+        userId: objects.map((object) => object.authorId),
+        limit: 100,
+      })
+    ).map(filterUserForClient);
+    // console.log(users);
+
+    return objects.map((object) => ({
+      object,
+      author: users.find((user) => user.id === object.authorId),
+    }));
   }),
 });
