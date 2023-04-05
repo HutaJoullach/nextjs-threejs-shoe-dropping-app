e19a619d40733ad3ee3e66ed155affab1b4e754e
HutaJoullach <hutajoullach@gmail.com>
2023-04-05 13:08:24 +0900

    added canvas-loader

--- src/components/canvas/cat.tsx
+++ src/components/canvas/cat.tsx
@@ -2,7 +2,7 @@ import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
-// import CanvasLoader from "../loader";
+import CanvasLoader from "./canvas-loader";
 
 type CatProps = {
   isMobile?: boolean | undefined;
