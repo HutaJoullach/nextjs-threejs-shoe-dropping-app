80d371d156e494d3a9a0423986ea858422050f15
HutaJoullach <hutajoullach@gmail.com>
2023-04-07 14:44:23 +0900

    added draco loaders

--- src/components/canvas/loaders.tsx
+++ src/components/canvas/loaders.tsx
@@ -1,3 +1,37 @@
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
+import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
 import * as THREE from "three";
+
+// draco
+const draco = new DRACOLoader();
+draco.setDecoderPath("../node_modules/three/examples/js/libs/draco/gltf/");
+draco.setDecoderConfig({ type: "js" });
+export const dracoLoader = draco;
+
+// gltf
+const gltf = new GLTFLoader();
+gltf.setDRACOLoader(dracoLoader);
+export const gltfLoader = gltf;
+
+// fix type errors
+const promisify = ({ loader, onProgress }: any) => {
+  const promiseLoader = ({ url }: any) => {
+    return new Promise((resolve, reject) => {
+      loader.load(url, resolve, onProgress, reject);
+    });
+  };
+  return {
+    originalLoader: loader,
+    load: promiseLoader,
+  };
+};
+
+export const fbxLoader = promisify(new FBXLoader());
+
+// import statement, usecase below
+// import { fbxLoader } from './loaders'
+
+// fbxLoader.load('cat.fbx').then(model => {
+//   console.log(model)
+// })
