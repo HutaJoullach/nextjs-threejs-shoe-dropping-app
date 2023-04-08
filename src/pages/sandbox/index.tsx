715515c726cdc510e72fba88e13de4c732f4bb9e
HutaJoullach <hutajoullach@gmail.com>
2023-04-08 20:10:57 +0900

    added css alert

--- src/pages/sandbox/index.tsx
+++ src/pages/sandbox/index.tsx
@@ -35,56 +35,96 @@ const CowControlButton = ({ isCowOpened, setIsCowOpened }) => {
 
   return (
     <div
-      className={`${theme.rounded.utilityCardBorder} ${theme.bg.utilityCardBackground} flex w-full gap-3 px-2 py-1`}
+      // className={`${theme.rounded.utilityCardBorder} flex w-full items-center justify-center gap-3 px-2 py-1`}
+      className={`${theme.rounded.utilityCardBorder} flex`}
     >
-      <Image
-        src={caticon}
-        alt="caticon"
-        className="h-6 w-6 rounded-full"
-        width={56}
-        height={56}
-      />
-
-      <button
-        onClick={() => {
-          if (!objectsLoading) api.objects.getAll.useQuery();
-        }}
-        disabled={objectsLoading}
+      <div
+        className="bg-warning-100 text-warning-800 hidden items-center rounded-lg text-xs data-[te-alert-show]:inline-flex"
+        role="alert"
+        data-te-alert-init
+        data-te-alert-show
       >
-        <div className="flex items-center justify-center">
-          {!objectsLoading ? (
-            <>
-              <span>fetch new shoes</span>
-              <Image
-                src={caticon}
-                className="h-5 w-5 rounded-full"
-                alt="caticon"
-                width={56}
-                height={56}
+        {/* <strong className="mr-1">Holy guacamole! </strong>  */}
+        got a shoe for me?
+        <button
+          type="button"
+          className="text-warning-900 hover:text-warning-900 rounded-none border-none opacity-50 hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none"
+          data-te-alert-dismiss
+          aria-label="Close"
+        >
+          <span className="w-[1em] focus:opacity-100 disabled:pointer-events-none disabled:select-none disabled:opacity-25 [&.disabled]:pointer-events-none [&.disabled]:select-none [&.disabled]:opacity-25">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="h-6 w-6"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z"
+                clip-rule="evenodd"
               />
-            </>
-          ) : (
-            <LoadingSpinner size={20} />
-          )}
-        </div>
-      </button>
+            </svg>
+          </span>
+        </button>
+      </div>
 
-      <button
-        onClick={() => {
-          if (!isCowOpened) setIsCowOpened(!isCowOpened);
-        }}
+      <div
+        className={`${theme.rounded.utilityCardBorder} flex w-full items-center justify-center gap-3 px-2 py-1`}
       >
-        <div className="flex items-center justify-center">
-          <span>add my shoe!</span>
-          <Image
-            src={caticon}
-            className="h-5 w-5 rounded-full"
-            alt="caticon"
-            width={56}
-            height={56}
-          />
-        </div>
-      </button>
+        <Image
+          src={caticon}
+          alt="caticon"
+          className="h-8 w-8 rounded-full border-2 border-red-200"
+          width={56}
+          height={56}
+        />
+
+        <button
+          onClick={() => {
+            if (!objectsLoading) api.objects.getAll.useQuery();
+          }}
+          disabled={objectsLoading}
+        >
+          <div
+            className={`${theme.rounded.utilityCardBorder} flex items-center justify-center gap-1 bg-zinc-500 px-2 py-1 text-xs`}
+          >
+            {!objectsLoading ? (
+              <>
+                <span>fetch new shoes</span>
+                <Image
+                  src={caticon}
+                  className="h-5 w-5 rounded-full"
+                  alt="caticon"
+                  width={56}
+                  height={56}
+                />
+              </>
+            ) : (
+              <LoadingSpinner size={20} />
+            )}
+          </div>
+        </button>
+
+        <button
+          onClick={() => {
+            if (!isCowOpened) setIsCowOpened(!isCowOpened);
+          }}
+        >
+          <div
+            className={`${theme.rounded.utilityCardBorder} flex items-center justify-center gap-1 bg-zinc-500 px-2 py-1 text-xs`}
+          >
+            <span>add my shoe!</span>
+            <Image
+              src={caticon}
+              className="h-5 w-5 rounded-full"
+              alt="caticon"
+              width={56}
+              height={56}
+            />
+          </div>
+        </button>
+      </div>
     </div>
   );
 };
