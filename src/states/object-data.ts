f0da08e11032f9f801f401330fa39b7cb53e111f
HutaJoullach <hutajoullach@gmail.com>
2023-04-10 00:15:41 +0900

    added object data state

--- src/states/object-data.ts
+++ src/states/object-data.ts
@@ -1,3 +1,16 @@
 import { atom } from "jotai";
 
 export const isDataRefetchedAtom = atom<boolean>(false);
+
+interface objectDataToMutate {
+  laces: string;
+  mesh: string;
+  caps: string;
+  inner: string;
+  sole: string;
+  stripes: string;
+  band: string;
+  patch: string;
+}
+
+export const objectDataToMutateAtom = atom<objectDataToMutate | null>(null);
