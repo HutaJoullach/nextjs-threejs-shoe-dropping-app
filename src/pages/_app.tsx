52c59e620fd288b43aa58511a7df2b3409d5c1fd
HutaJoullach <hutajoullach@gmail.com>
2023-04-09 15:54:25 +0900

    added jotai provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,18 +5,21 @@ import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Provider as JotaiProvider } from "jotai";
 import { Toaster } from "react-hot-toast";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
-      <Head>
-        <title>Next.js, Three.js Sandbox App</title>
-        <meta name="description" content="t3-threejs-sandbox-app" />
-        <link rel="icon" href="/alphabet.svg" />
-      </Head>
-      <Toaster position="top-right" />
-      <Component {...pageProps} />
+      <JotaiProvider>
+        <Head>
+          <title>Next.js, Three.js Sandbox App</title>
+          <meta name="description" content="t3-threejs-sandbox-app" />
+          <link rel="icon" href="/alphabet.svg" />
+        </Head>
+        <Toaster position="top-right" />
+        <Component {...pageProps} />
+      </JotaiProvider>
     </ClerkProvider>
   );
 };
