42d00acb2e0b8b549e8e99e9db0d5509937c702b
HutaJoullach <hutajoullach@gmail.com>
2023-04-25 09:12:09 +0900

    fixed publlic url for next.js naming

--- src/pages/sandbox/index.tsx
+++ src/pages/sandbox/index.tsx
@@ -478,12 +478,14 @@ const Scene = () => {
     return () => window.removeEventListener("keydown", keydownHandler);
   }, [thirdPerson]);
 
+  console.log(`here! ${process.env.NEXT_PUBLIC_URL}`);
+
   return (
     <Suspense fallback={null}>
       <Environment
         files={
-          process.env.PUBLIC_URL
-            ? `${process.env.PUBLIC_URL}/textures/vignaioli.hdr`
+          process.env.NEXT_PUBLIC_URL
+            ? `${process.env.NEXT_PUBLIC_URL}/textures/vignaioli.hdr`
             : `http://localhost:3000/textures/vignaioli.hdr`
         }
         background={"only"}
