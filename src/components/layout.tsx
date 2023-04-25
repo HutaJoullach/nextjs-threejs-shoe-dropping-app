7a008a86f655102a4ece55065436ae9b4540d16e
HutaJoullach <hutajoullach@gmail.com>
2023-04-25 15:36:39 +0900

    added email submit logic

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,8 +1,12 @@
+import { useState } from "react";
 import type { PropsWithChildren } from "react";
 
 import theme from "../styles/styles";
 import Navbar from "./navbar";
 
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import emailjs from "@emailjs/browser";
+import { toast } from "react-hot-toast";
 import { IoMdClose } from "react-icons/io";
 
 import { useAtom } from "jotai";
@@ -10,6 +14,79 @@ import { isContactModalOpenedAtom } from "../states/object-data";
 import { useHydrateAtoms } from "jotai/utils";
 
 const ContactModal = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    formState: { errors },
+    reset,
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      message: "",
+    },
+  });
+
+  const name = watch("name");
+  const email = watch("email");
+  const message = watch("message");
+
+  const setCustomValue = (id: string, value: any) => {
+    setValue(id, value, {
+      shouldDirty: true,
+      shouldTouch: true,
+      shouldValidate: true,
+    });
+  };
+
+  const onSubmit: SubmitHandler<FieldValues> = (data: any) => {
+    setIsLoading(true);
+
+    if (
+      !process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID ||
+      !process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID ||
+      !process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY ||
+      !process.env.NEXT_PUBLIC_MY_EMAIL_ADDRESS
+    ) {
+      toast.error("Something went wrong.");
+      setIsLoading(false);
+      return;
+    }
+
+    emailjs
+      .send(
+        process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
+        process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
+        {
+          from_name: name,
+          to_name: "shoe loving cat",
+          from_email: email,
+          to_email: process.env.NEXT_PUBLIC_MY_EMAIL_ADDRESS,
+          message: message,
+        },
+        process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY
+      )
+      .then(
+        () => {
+          toast.success("email sent!");
+          reset();
+        },
+        (error) => {
+          toast.error("Something went wrong.");
+          reset();
+        }
+      )
+      .finally(() => {
+        setIsLoading(false);
+        if (isContactModalOpened)
+          setIsContactModalOpened(!isContactModalOpened);
+      });
+  };
+
   useHydrateAtoms([[isContactModalOpenedAtom, false] as const]);
   const [isContactModalOpened, setIsContactModalOpened] = useAtom(
     isContactModalOpenedAtom
@@ -22,7 +99,7 @@ const ContactModal = () => {
       <div
         className={`${theme.bg.contactModalBackground} ${theme.rounded.utilityCardBorder} relative mx-auto my-6 h-full w-full p-5 md:h-auto md:w-4/6 lg:h-auto lg:w-3/6 xl:w-2/5`}
       >
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="relative flex items-center justify-center rounded-t p-4">
             <button
               className="absolute right-6 border-0 p-1 text-white transition hover:opacity-70"
@@ -39,8 +116,10 @@ const ContactModal = () => {
             <input
               type="text"
               name="name"
-              value=""
-              onChange={() => {}}
+              value={name}
+              onChange={(name) => {
+                setCustomValue("name", name);
+              }}
               placeholder="name"
               className="bg-tertiary placeholder:text-secondary rounded-lg border-none px-6 py-4 font-medium text-white outline-none"
             />
@@ -50,8 +129,10 @@ const ContactModal = () => {
             <input
               type="email"
               name="email"
-              value=""
-              onChange={() => {}}
+              value={email}
+              onChange={(email) => {
+                setCustomValue("email", email);
+              }}
               placeholder="email"
               className="bg-tertiary placeholder:text-secondary rounded-lg border-none px-6 py-4 font-medium text-white outline-none"
             />
@@ -61,8 +142,10 @@ const ContactModal = () => {
             <textarea
               rows={7}
               name="message"
-              value=""
-              onChange={() => {}}
+              value={message}
+              onChange={(message) => {
+                setCustomValue("message", message);
+              }}
               placeholder="message"
               className="bg-tertiary placeholder:text-secondary rounded-lg border-none px-6 py-4 font-medium text-white outline-none"
             />
@@ -70,10 +153,11 @@ const ContactModal = () => {
           <span className="py-3"></span>
           <button
             type="submit"
+            disabled={true}
             className="bg-tertiary shadow-primary absolute right-6 w-fit rounded-xl px-8 py-3 font-bold text-white shadow-md outline-none"
           >
-            {/* {false ? "sending..." : "send"} */}
-            `scaffolding 🚧👷🏼‍♂️🏗️`
+            {/* {isLoading ? "sending..." : "send"} */}
+            <span>scaffolding 🚧👷🏼‍♂️🏗️</span>
           </button>
         </form>
       </div>
