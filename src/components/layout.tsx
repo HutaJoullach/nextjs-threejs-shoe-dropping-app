7bf5c635d47bcd2467af0d77facebba7c154303d
HutaJoullach <hutajoullach@gmail.com>
2023-04-25 13:37:36 +0900

    contact modal style

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -20,18 +20,18 @@ const ContactModal = () => {
       className={`fixed inset-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden bg-neutral-800/70 outline-none focus:outline-none`}
     >
       <div
-        className={`${theme.bg.contactModalBackground} ${theme.rounded.utilityCardBorder} relative mx-auto my-6 h-full w-full p-3 md:h-auto md:w-4/6 lg:h-auto lg:w-3/6 xl:w-2/5`}
+        className={`${theme.bg.contactModalBackground} ${theme.rounded.utilityCardBorder} relative mx-auto my-6 h-full w-full p-5 md:h-auto md:w-4/6 lg:h-auto lg:w-3/6 xl:w-2/5`}
       >
         <form>
           <div className="relative flex items-center justify-center rounded-t p-4">
             <button
-              className="absolute right-9 border-0 p-1 text-white transition hover:opacity-70"
+              className="absolute right-6 border-0 p-1 text-white transition hover:opacity-70"
               onClick={() => {
                 if (isContactModalOpened)
                   setIsContactModalOpened(!isContactModalOpened);
               }}
             >
-              <IoMdClose size={18} />
+              <IoMdClose size={24} />
             </button>
           </div>
           <label className="flex flex-col">
@@ -69,7 +69,7 @@ const ContactModal = () => {
           </label>
           <button
             type="submit"
-            className="bg-tertiary shadow-primary w-fit rounded-xl px-8 py-3 font-bold text-white shadow-md outline-none"
+            className="bg-tertiary shadow-primary right-6 w-fit rounded-xl px-8 py-3 font-bold text-white shadow-md outline-none"
           >
             {false ? "sending..." : "send"}
           </button>
